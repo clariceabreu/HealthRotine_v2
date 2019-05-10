79c601b08d9e41160d9fa23c6ea44040685f8d57
Clarice <clacla1998@gmail.com>
2019-05-09 23:36:54 -0300

    creating layout for type medical consultation

--- app/src/main/java/com/fsi/healthrotine/MainActivity.java
+++ app/src/main/java/com/fsi/healthrotine/MainActivity.java
@@ -8,12 +8,20 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
-    private String[] types = new String[]{"Consulta", "Exame", "Remédio"};
+    private String[] typesArray = new String[]{"Selecione", "Consulta", "Exame", "Remédio"};
+    private String[] specialtiesArray = new String[]{"Selecione","Clínica Médica", "Oftomologia", "Otorrinologia", "Pediatria", "Ginebologia"};
+    private Integer[] daysArray = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
+    private Integer[] monthsArray = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    private Integer[] yearsArray = new Integer[200];
+    private Integer[] hoursArray = new Integer[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23};
+    private Integer[] minutesArray = new Integer[61];
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -43,19 +51,101 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        //Tipo
+        Spinner spinnerType = (Spinner) findViewById(R.id.spinnerType);
         // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, types);
+        ArrayAdapter<String> adapterType = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, typesArray);
         // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapterType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
+        spinnerType.setAdapter(adapterType);
 
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+        //Especialidade
+        final TextView textViewSpecialy = (TextView) findViewById(R.id.textViewSpecialty);
+
+        final Spinner spinnerSpecialty = (Spinner) findViewById(R.id.spinnerSpecialty);
+        ArrayAdapter<String> adapterSpecialty = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, specialtiesArray);
+        adapterSpecialty.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerSpecialty.setAdapter(adapterSpecialty);
+
+
+        //Data
+        final TextView textViewDate = (TextView) findViewById(R.id.textViewDate);
+
+        final Spinner spinnerDay = (Spinner) findViewById(R.id.spinnerDay);
+        ArrayAdapter<Integer> adapterDay = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_dropdown_item, daysArray);
+        adapterDay.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerDay.setAdapter(adapterDay);
+
+        final Spinner spinnerMonth = (Spinner) findViewById(R.id.spinnerMonth);
+        ArrayAdapter<Integer> adapterMonth = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_dropdown_item, monthsArray);
+        adapterMonth.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerMonth.setAdapter(adapterMonth);
+
+        int y = 1900;
+        for (int i = 0; i < 100; i++){
+            yearsArray[i] = (y);
+            y++;
+        }
+
+        final Spinner spinnerYear = (Spinner) findViewById(R.id.spinnerYear);
+        ArrayAdapter<Integer> adapterYear = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_dropdown_item, yearsArray);
+        adapterYear.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerYear.setAdapter(adapterYear);
+
+
+        //Hora
+        final TextView textViewHour = (TextView) findViewById(R.id.textViewHour);
+
+        final Spinner spinnerHour = (Spinner) findViewById(R.id.spinnerHour);
+        ArrayAdapter<Integer> adapterHour = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_dropdown_item, hoursArray);
+        adapterHour.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerHour.setAdapter(adapterHour);
+
+        for (Integer i = 0; i < 61; i++){
+            minutesArray[i] = i;
+        }
+
+        final Spinner spinnerMinute = (Spinner) findViewById(R.id.spinnerMinute);
+        ArrayAdapter<Integer> adapterMinute = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_dropdown_item, minutesArray);
+        adapterMinute.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerMinute.setAdapter(adapterMinute);
+
+
+        //Comentário
+        final TextView textViewComment = (TextView) findViewById(R.id.textViewComment);
+        final EditText editTextComment = (EditText) findViewById(R.id.editTextComment);
+
+
+        //Botão
+        final Button buttonCreate = (Button) findViewById(R.id.buttonCreate);
+
+
+        spinnerType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                // Handle
+                if (position == 1){
+                    textViewSpecialy.setVisibility(View.VISIBLE);
+                    spinnerSpecialty.setVisibility(View.VISIBLE);
+                    textViewDate.setVisibility(View.VISIBLE);
+                    spinnerDay.setVisibility(View.VISIBLE);
+                    spinnerMonth.setVisibility(View.VISIBLE);
+                    spinnerYear.setVisibility(View.VISIBLE);
+                    textViewHour.setVisibility(View.VISIBLE);
+                    spinnerHour.setVisibility(View.VISIBLE);
+                    spinnerMinute.setVisibility(View.VISIBLE);
+                    textViewComment.setVisibility(View.VISIBLE);
+                    editTextComment.setVisibility(View.VISIBLE);
+                    buttonCreate.setVisibility(View.VISIBLE);
+
+                } else {
+                    textViewSpecialy.setVisibility(View.INVISIBLE);
+                    spinnerSpecialty.setVisibility(View.INVISIBLE);
+                }
+
+
             }
 
             @Override

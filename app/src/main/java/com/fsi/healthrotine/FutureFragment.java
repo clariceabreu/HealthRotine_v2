fb2dff390664f63d2c2d5088920ed78486cd682b
Clarice Abreu <clarice.abreu@medcel.com.br>
2019-06-10 18:07:11 -0300

    fixing bug on filters

--- app/src/main/java/com/fsi/healthrotine/FutureFragment.java
+++ app/src/main/java/com/fsi/healthrotine/FutureFragment.java
@@ -42,7 +42,7 @@ public class FutureFragment extends Fragment {
     private DataBase db;
     private LinearLayout layout;
     private String[] typesArray = new String[]{"Ambos", "Consulta", "Remédio"};
-    private String[] specialtiesArray = new String[]{"Todas","Clínica Médica", "Oftomologia", "Otorrinologia", "Pediatria", "Ginebologia"};
+    private String[] specialtiesArray = new String[]{"Selecione","Acupuntura", "Alergia e Imunologia", "Anestesiologia", "Angiologia", "Cancerologia", "Cardiologia", "Cirurgia Cardiovascular", "Cirurgia da Mão", "Cirurgia de Cabeça e Pescoço", "Cirurgia do Aparelho Digestivo", "Cirurgia Geral", "Cirurgia Pediátrica", "Cirurgia Plástica", "Cirurgia Torácica", "Cirurgia Vascular", "Clínica Médica", "Coloproctologia", "Dermatologia", "Endocrinologia e Metabologia", "Endoscopia", "Gastroenterologia", "Genética Médica", "Geriatria", "Ginecologia e Obstetrícia", "Hematologia e Hemoterapia", "Homeopatia", "Infectologia", "Mastologia", "Medicina de Família e Comunidade", "Medicina do Trabalho", "Medicina de Tráfego", "Medicina Esportiva", "Medicina Física e Reabilitação", "Medicina Intensiva", "Medicina Legal e Perícia Médica", "Medicina Nuclear", "Medicina Preventiva e Social", "Nefrologia", "Neurocirurgia", "Neurologia", "Nutrologia", "Oftalmologia", "Ortopedia e Traumatologia", "Otorrinolaringologia", "Patologia", "Patologia Clínica/Medicina Laboratorial", "Pediatria", "Pneumologia", "Psiquiatria", "Radiologia e Diagnóstico por Imagem", "Radioterapia", "Reumatologia", "Urologia"};
 
 
     public FutureFragment() {
